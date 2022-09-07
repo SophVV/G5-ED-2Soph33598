d717b6c7c8f98b5e0a56fb5851c2fba7ee7fa6a1
Soph <sophia.vazquez@cetys.edu.mx>
2022-09-06 18:38:20 -0700

    Tareas

---
+++ KIUBO RAZA/"Kiubo razo".cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kiubo raza");
+            Console.WriteLine("Hello World!");
+
+// Console.WriteLine("Hello World!");
+
+            Console.Write("Congratulations!");
+            Console.Write(" ");
+            Console.Write("You wrote your first lines of code!");
+            
+            Console.WriteLine("This is the first line.");
+
+            Console.Write("This is the ");
+            Console.Write("second line.");
+
+
+        }
+    }
+}
+        
